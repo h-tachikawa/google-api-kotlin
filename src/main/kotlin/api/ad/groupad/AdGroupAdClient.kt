1cbf1ce467c94d5b38322447d6f9c1cfdf136ec9
tachikawa <tachikawa@feedforce.jp>
2021-12-04 23:21:29 +0900

    feat: implement AdGroupAdClient

---
+++ src/main/kotlin/api/ad/groupad/AdGroupAdClient.kt
@@ -0,0 +1,36 @@
+package api.ad.groupad
+
+import com.google.ads.googleads.lib.GoogleAdsClient
+import com.google.ads.googleads.v9.services.GoogleAdsServiceClient
+import com.google.ads.googleads.v9.services.SearchGoogleAdsRequest
+import com.google.ads.googleads.v9.utils.ResourceNames
+
+class AdGroupAdClient {
+    companion object {
+        private val client = GoogleAdsClient.newBuilder()
+            .fromEnvironment()
+            .fromPropertiesFile()
+            .build()
+            .latestVersion
+            .createGoogleAdsServiceClient()
+
+        fun get(customerId: Long, adGroupId: Long, adGroupAdId: Long): GoogleAdsServiceClient.SearchPagedResponse? {
+            val query = """
+                SELECT
+                  ad_group_ad.status,
+                  ad_group_ad.policy_summary.approval_status
+                FROM
+                  ad_group_ad
+                WHERE
+                  ad_group_ad.resource_name = '${ResourceNames.adGroupAd(customerId, adGroupId, adGroupAdId)}'
+            """
+
+            val request = SearchGoogleAdsRequest.newBuilder()
+                .setCustomerId(customerId.toString())
+                .setQuery(query)
+                .build()
+
+            return client.search(request)
+        }
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,51 @@
+import api.ad.groupad.AdGroupAdClient
 import api.datafeed.DatafeedClient
 import api.datafeed.status.DatafeedStatusClient
 
 fun main(args: Array<String>) {
+    adsApiOps(args)
+}
 
+fun adsApiOps(args: Array<String>) {
+    val customerIdStr = args[0]
+    val adGroupIdStr = args[1]
+    val adGroupAdIdStr = args[2]
+
+    val customerId = try {
+        customerIdStr.toLong()
+    } catch (e: NumberFormatException) {
+        null
+    }
+
+    val adGroupId = try {
+        adGroupIdStr.toLong()
+    } catch (e: NumberFormatException) {
+        null
+    }
+
+    val adGroupAdId = try {
+        adGroupAdIdStr.toLong()
+    } catch (e: NumberFormatException) {
+        null
+    }
+
+    if (customerId == null || adGroupId == null || adGroupAdId == null) {
+        println("some required arguments is empty.")
+        return
+    }
+
+    val res = AdGroupAdClient.get(customerId, adGroupId, adGroupAdId) ?: return
+
+    val adGroupAdList = res.iterateAll().map {
+        it.adGroupAd
+    }
+
+    adGroupAdList.forEach {
+        println(it.toString())
+    }
+}
+
+fun contentApiOps(args: Array<String>) {
     val merchantIdStr = args[0]
     val datafeedIdStr = args[1]
 
@@ -37,6 +80,6 @@ fun main(args: Array<String>) {
     )
 
     if (datafeedStatus != null) {
-        println(datafeedStatus.lastUploadDate)
+        println(datafeedStatus)
     }
 }
